620eb1ac552d313d77446729e0202027ed214108
Dipanjan777777 <dipanjansantra2019@gmail.com>
2025-10-25 19:43:20 +0530

    EDA partially done

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> list[str]:
+    '''This function will return the list of requirements'''
+    requirements = []
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+   name='Strategic-Sales-Forecasting-Full-Spectrum-Sales---Demand-Forecasting',
+   version='0.0.1',
+   description='A useful module',
+   author='Dipanjan Santra',
+   author_email='dipanjansantra2019@gmail.com',
+   packages=find_packages(),  # Automatically find packages
+   install_requires=get_requirements('requirements.txt'),  # Parse dependencies from requirements.txt
+)
